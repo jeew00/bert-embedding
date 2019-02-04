40c853c95a2192f61ef4d12c2cd3de543ed340b3
jeew <jeew@laptop.local>
2019-02-04 14:51:57 +0700

    Fix readme and add test size argument

--- evaluate.py
+++ evaluate.py
@@ -19,17 +19,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--nparray_path', required=True, type=str, help="Numpy array file (.npy) of pre-embed sentences path")
 parser.add_argument('--excel_path', required=True, type=str, help="Excel dataset file path")
 parser.add_argument('--thai_model', action='store_true', help="For thai model")
+parser.add_argument('test_size', default=0.25, help="Test set size")
 args = parser.parse_args()
 
 
 random_state = 0
-test_size = 0.001
 
 
 bc = BertClient()
 df = pd.read_excel(args.excel_path)
 df['bert'] = np.load(args.nparray_path).tolist()
-train_set, test_set = train_test_split(df, test_size=test_size, shuffle=True, random_state=random_state)
+train_set, test_set = train_test_split(df, test_size=args.test_size, shuffle=True, random_state=random_state)
 
 feat = np.vstack(train_set['bert'].values)
 nfeat = normalize(feat)
@@ -55,7 +55,7 @@ def evaluate_test():
     bertRes = []
     editRes = []
     # cut_test_set = test_set
-    print('Evaluate on test set:\t{}\texamples'.format(int(df.shape[0] * test_size)))
+    print('Evaluate on test set: {} examples'.format(int(df.shape[0] * args.test_size)))
     for i in trange(len(test_set)):
         ts = test_set.iloc[i]['Keyword']
         bertRes.append(query_nearest(ts, thai_model=args.thai_model))
@@ -67,10 +67,10 @@ def evaluate_test():
     accEdit = sum(test_set['edit_result'] == test_set['Topic'])/len(test_set)
 
     print('Random state:\t{}'.format(random_state))
-    print("Train set's size: {} ({}%)".format(int(train_set.shape[0]), (1 - test_size) * 100))
-    print("Test set's size: {} ({}%)".format(int(test_set.shape[0]), test_size * 100))
-    print('{} acc:\t{}'.format(args.nparray_path.split('/')[-1], accBert))
-    print('Edit acc:\t{}'.format(accEdit))
+    print("Train set's size: {} ({}%)".format(int(train_set.shape[0]), (1 - args.test_size) * 100))
+    print("Test set's size: {} ({}%)".format(int(test_set.shape[0]), args.test_size * 100))
+    print('{} acc: {}'.format(args.nparray_path.split('/')[-1], accBert))
+    print('Edit acc: {}'.format(accEdit))
     bc.close()
     # return accBert,accEdit
 
